00a5179fd8965d22d755641ce780904c7c1d281b
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-03-22 20:53:37 +0500

    Делаю диалог, урок 29 минута 7.17 - продолжить

--- app/src/main/java/com/example/fitnessapp/fragments/DaysFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragments/DaysFragment.kt
@@ -3,6 +3,9 @@ package com.example.fitnessapp.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBar
@@ -16,12 +19,18 @@ import com.example.fitnessapp.adapter.ExerciseModel
 import com.example.fitnessapp.databinding.FragmentDaysBinding
 import com.example.fitnessapp.utils.FragmentManager
 import com.example.fitnessapp.utils.MainViewModel
-import java.util.zip.Inflater
 
 class DaysFragment : Fragment(), DaysAdapter.Listener { // Подключили интерфейс из который создали в DaysAdapter
+    private lateinit var adapter : DaysAdapter  // мы вынесли адаптер сюда, чтобы ОБНОВИТЬ шаред префс по факту как только пользователь нажал очистить
     private lateinit var binding: FragmentDaysBinding
     private val model: MainViewModel by activityViewModels() // Добавили зависимость. Для добавления надо указать зависимость от фрагмент в Gradle !
     private var ab: ActionBar? = null // добавили переменную для ActionBar, будем показывать счетчик упражнений
+
+    override fun onCreate(savedInstanceState: Bundle?) {  // мы включили меню, его надо включить!
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)  // deprecated too
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,8 +50,25 @@ model.currentDay = 0     // мы обнуляем currentDay для того, ч
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) { // это deprecated - узнай чем заменить
+    return inflater.inflate(R.menu.main_menu, menu)  // Сделали Меню - теперь есть 3 точки чтобы сбросить
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Deprecated !!!!!
+        if (item.itemId == R.id.reset) { // Мы проверили, если мы НАЖАЛИ именно на ВАРИАНТ "СБРОСИТЬ" - то выполняем действия в скобках
+            model.pref?.edit()?.clear()?.apply()  // стираем всю таблицу - всё сбрасываем и делаем заново ( если пользователь решил начать с начала)
+            adapter.submitList(fillDaysArray())  // мы специально ВЫНЕСЛИ адатер в Лейт Инит Вар чтобы использовать сдесь и обновить адаптер если пользователь нажмет СБРОС
+
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
     private fun initRcView() = with(binding){
-        val adapter = DaysAdapter(this@DaysFragment)
+        adapter = DaysAdapter(this@DaysFragment)
         rcviewdays.layoutManager = LinearLayoutManager(activity as AppCompatActivity)
         rcviewdays.adapter = adapter
         adapter.submitList(fillDaysArray())
@@ -51,12 +77,24 @@ model.currentDay = 0     // мы обнуляем currentDay для того, ч
 
     private fun fillDaysArray() : ArrayList<DayModel>{
         val tArray = ArrayList<DayModel>() // создаём класс для заполнения из массивов с упражнениями. получается массив с упражнениями который состоит из DayModel
+        var daysDoneCounter = 0
     resources.getStringArray(R.array.day_exercise).forEach {
   model.currentDay++
  val exCounter = it.split(",").size  // мы делаем чек бокс на экране с упражнениями. Он будет заполняться только когда ВСЕ упражнения данного дня - выполнены. Для этого возьмем массив, разделим по запятокй у узнаем размер, чтобы сравнить со счетчиком
     tArray.add(DayModel(it, model.getExerciseCount() == exCounter, 0)) // вот здесь я передал пока что 0 потому что добавленное поле DayModel требует указать все поля в конструкторе
     }
+        binding.progressbar.max = tArray.size // размер массива это максимум проресс бара
+        tArray.forEach{
+            if (it.isDone) daysDoneCounter++ // Делаю прогресс бар ( сколько выполнено дней) - для этого сделал переменную restDaysCounter, которая считает по циклу количество isDone
+        }
+        updateRestDaysUI(tArray.size- daysDoneCounter, tArray.size) // - считаем оставшиеся дни + Обновляем прогрессбар
     return tArray
+    }
+
+    private fun updateRestDaysUI(restDays : Int, days: Int) = with(binding){
+        val rDays = getString(R.string.rest)+ " $restDays "+ getString(R.string.rest_days) // собрали строку, сколько осталось дней ) на главном экране
+        tvRestDays.text = rDays
+        progressbar.progress = days - restDays // покажем прогресс в прогресс баре (DaysFragment)
     }
 
     private fun fillExerciseList(day: DayModel) {
