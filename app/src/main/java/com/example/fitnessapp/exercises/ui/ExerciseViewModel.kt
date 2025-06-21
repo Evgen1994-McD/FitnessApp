833fc20b27d4faabcdbbf1ab41d4755a5f1adb19
Evgen1994-McD <mrmarwell@gmail.com>
2025-06-21 17:10:24 +0500

    урок 25

---
+++ app/src/main/java/com/example/fitnessapp/exercises/ui/ExerciseViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.fitnessapp.exercises.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitnessapp.db.DayModel
+import com.example.fitnessapp.db.MainDb
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExerciseViewModel @Inject constructor(
+private val mainDb: MainDb
+) : ViewModel() {
+    var currentDay: DayModel? = null
+    private fun updateDay(dayModel: DayModel) = viewModelScope.launch {
+        mainDb.daysDao.insertDay(dayModel)
+    }
+
+    private fun dayDone(){
+        currentDay = currentDay?.copy(isDone = true)
+        currentDay?.let {
+            updateDay(it)
+        }
+/*
+currentDay передаём тот же, но перезапишем параметр isDone чтобы поставить галочку
+ */
+    }
+
+    fun getExercises(dayModel: DayModel) {
+        currentDay = dayModel
+        viewModelScope.launch {
+            val exerciseList = mainDb.exerciseDao.getAllExercises()
+        }
+
+    }
+}
