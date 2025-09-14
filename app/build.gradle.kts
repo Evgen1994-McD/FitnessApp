8182597f485e48c277bceeae0d220b81f892d2db
Evgen1994-McD <mrmarwell@gmail.com>
2025-09-15 00:30:50 +0500

    иконка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.example.fitnessapp"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
