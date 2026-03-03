797fe085deb7ee1dc83d7283d3625c8f6d9941a6
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2026-03-03 18:56:56 +0500

    реклама

--- app/src/main/java/com/example/fitnessapp/utils/App.kt
+++ app/src/main/java/com/example/fitnessapp/utils/App.kt
@@ -12,8 +12,8 @@ class App:Application() {
     companion object {
         // Демонстрационный ID для тестирования
         // Замените на реальный ID из Рекламной сети Яндекса перед релизом
-        private const val APP_OPEN_AD_UNIT_ID = "R-M-18059682-1"
-        
+        private const val APP_OPEN_AD_UNIT_ID = "R-M-18846080-1"
+
         @Volatile
         private var appOpenAdManager: AppOpenAdManager? = null
         
