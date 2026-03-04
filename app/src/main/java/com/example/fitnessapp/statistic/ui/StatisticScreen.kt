5768431746127e6b985caf86119a3e313e8e8a5b
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2026-03-04 20:19:28 +0500

    Работаем над новой статистикой

--- app/src/main/java/com/example/fitnessapp/statistic/ui/StatisticScreen.kt
+++ app/src/main/java/com/example/fitnessapp/statistic/ui/StatisticScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Height
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -92,7 +93,7 @@ fun NewStatisticScreen(
                         onClick = { /* Уведомления */ }
                     ) {
                         Icon(
-                            imageVector = Icons.Default.Notifications,
+                            imageVector = Icons.Default.Height,
                             contentDescription = "Уведомления",
                             tint = MaterialTheme.colorScheme.onSurface
                         )
